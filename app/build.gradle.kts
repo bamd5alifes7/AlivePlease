825edf0059c3adc78a56cab901a44cfd51dc9d00
CapyzZ <103618768+bamd5alifes7@users.noreply.github.com>
2026-05-29 00:11:49 +0800

    更新版本號

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.orenhui.aliveplease"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
