98c84215350fecd9ff835aa22081c0d85ae78741
CapyzZ <103618768+bamd5alifes7@users.noreply.github.com>
2026-06-12 12:31:56 +0800

    版號調整、關懷文字微調

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.orenhui.aliveplease"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.8"
+        versionCode = 9
+        versionName = "1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
